bea5c54bf4ffb9d179dcf21f6e30c18f25cabe7a
Jack Jaguar <j.jaguar55@gmail.com>
2018-11-29 18:02:57 +0200

    Start HW 5, added router, controller and form

---
+++ app/code/GumennoyEL/FourthModule/Controller/QuestionSubmit/Index.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace GumennoyEL\FourthModule\Controller\QuestionSubmit;
+
+use Magento\Framework\Controller\ResultFactory;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+
+    /**
+     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\ResultInterface
+     */
+
+    public function execute()
+    {
+
+    }
+
+
+}
---
+++ app/code/GumennoyEL/FourthModule/registration.php
@@ -0,0 +1,8 @@
+<?php
+
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'GumennoyEL_FourthModule',
+    __DIR__
+);
