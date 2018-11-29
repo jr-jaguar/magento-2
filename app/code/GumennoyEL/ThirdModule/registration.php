5b48df79b382ff6b0126e0147056c53ce8148910
Jack Jaguar <j.jaguar55@gmail.com>
2018-11-29 16:56:31 +0200

    HW4 Done

---
+++ app/code/GumennoyEL/ThirdModule/registration.php
@@ -0,0 +1,8 @@
+<?php
+
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'GumennoyEL_ThirdModule',
+    __DIR__
+);
---
+++ app/code/GumennoyEL/ThirdModule/view/frontend/requirejs-config.js
@@ -0,0 +1,7 @@
+var config = {
+    map: {
+        '*': {
+            popupDealerForm: 'GumennoyEL_ThirdModule/js/dealer_popup',
+        }
+    }
+};
